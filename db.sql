9993470bca0333676798d3c46dc45fe3642ffa00
Julen Arostegi <jul_aro_aye@goierrieskola.org>
2026-03-01 16:42:32 +0100

    Aplikazioa amaituta eta DB eguneratuta

--- db.sql
+++ db.sql
@@ -31,7 +31,7 @@ CREATE TABLE `erreserba` (
   PRIMARY KEY (`erreserbaId`),
   KEY `pelikulaId` (`pelikulaId`),
   CONSTRAINT `erreserba_ibfk_1` FOREIGN KEY (`pelikulaId`) REFERENCES `pelikula` (`pelikulaId`) ON DELETE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `erreserba` (
 
 LOCK TABLES `erreserba` WRITE;
 /*!40000 ALTER TABLE `erreserba` DISABLE KEYS */;
-INSERT INTO `erreserba` VALUES (1,1,'Aitor Etxeberria',3,'2026-02-10 18:30:00'),(2,1,'Maialen Garai',2,'2026-02-10 19:00:00'),(3,1,'Jon Aranburu',5,'2026-02-10 19:15:00'),(4,2,'Ane Lasa',4,'2026-02-11 17:00:00'),(5,2,'Mikel Sanz',1,'2026-02-11 17:10:00'),(6,3,'Leire Agirre',2,'2026-02-12 20:00:00'),(7,3,'Unai Mendizabal',5,'2026-02-12 20:05:00'),(8,3,'Iker Ruiz',3,'2026-02-12 20:10:00'),(9,4,'Nerea Lopez',5,'2026-02-13 21:00:00'),(10,4,'Ander Perez',5,'2026-02-13 21:05:00'),(11,4,'June Garcia',5,'2026-02-13 21:10:00'),(12,4,'Asier Torres',5,'2026-02-13 21:15:00'),(13,5,'Ibai Fernandez',2,'2026-02-14 18:00:00'),(14,6,'Irati Alonso',4,'2026-02-15 16:00:00'),(15,7,'Gorka Martinez',1,'2026-02-15 22:00:00');
+INSERT INTO `erreserba` VALUES (1,1,'Aitor Etxeberria',3,'2026-02-10 18:30:00'),(2,1,'Maialen Garai',2,'2026-02-10 19:00:00'),(3,1,'Jon Aranburu',5,'2026-02-10 19:15:00'),(4,2,'Ane Lasa',4,'2026-02-11 17:00:00'),(5,2,'Mikel Sanz',1,'2026-02-11 17:10:00'),(6,3,'Leire Agirre',2,'2026-02-12 20:00:00'),(7,3,'Unai Mendizabal',5,'2026-02-12 20:05:00'),(8,3,'Iker Ruiz',3,'2026-02-12 20:10:00'),(9,4,'Nerea Lopez',5,'2026-02-13 21:00:00'),(10,4,'Ander Perez',5,'2026-02-13 21:05:00'),(11,4,'June Garcia',5,'2026-02-13 21:10:00'),(12,4,'Asier Torres',5,'2026-02-13 21:15:00'),(13,5,'Ibai Fernandez',2,'2026-02-14 18:00:00'),(14,6,'Irati Alonso',4,'2026-02-15 16:00:00'),(15,7,'Gorka Martinez',1,'2026-02-15 22:00:00'),(16,1,'Andoni',5,'2026-02-28 14:29:50'),(17,1,'Maialen',3,'2026-02-28 18:01:47');
 /*!40000 ALTER TABLE `erreserba` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,7 +58,7 @@ CREATE TABLE `pelikula` (
   `eserlekuGuztira` int NOT NULL,
   `ezabatuta` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`pelikulaId`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +67,7 @@ CREATE TABLE `pelikula` (
 
 LOCK TABLES `pelikula` WRITE;
 /*!40000 ALTER TABLE `pelikula` DISABLE KEYS */;
-INSERT INTO `pelikula` VALUES (1,'Avatar','Zientzia fikziozko pelikula epikoa',100,0),(2,'Inception','Ametsen barruan gertatzen den thriller psikologikoa',80,0),(3,'Interstellar','Espazioan zehar bidaia gizateriaren etorkizuna salbatzeko',120,0),(4,'Titanic','Drama erromantikoa eta historikoa',150,0),(5,'The Dark Knight','Batmanen istorio iluna Jokerrekin',90,0),(6,'Frozen','Animaziozko pelikula familiarra',60,0),(7,'Matrix','Errealitate birtualaren inguruko zientzia fikzioa',75,0),(8,'Gladiator','Erromatar inperioan kokatutako akzio drama',110,1);
+INSERT INTO `pelikula` VALUES (1,'Avatar','Zientzia fikziozko pelikula epikoa',95,0),(2,'Inception','Ametsen barruan gertatzen den thriller psikologikoa',80,0),(3,'Interstellar 2','Espazioan zehar bidaia gizateriaren etorkizuna salbatzeko',120,0),(4,'Titanic','Drama erromantikoa eta historikoa',150,0),(5,'The Dark Knight','Batmanen istorio iluna Jokerrekin',90,0),(6,'Frozen','Animaziozko pelikula familiarra',60,1),(7,'Matrix','Errealitate birtualaren inguruko zientzia fikzioa',2,0),(8,'Gladiator','Erromatar inperioan kokatutako akzio drama',110,0);
 /*!40000 ALTER TABLE `pelikula` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -80,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-02-16 13:15:46
+-- Dump completed on 2026-03-01 16:42:06
